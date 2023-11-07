107cd9e6105370f45ebdaecb8aa95acafdee303e
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2023-11-07 13:49:32 -0500

    Added a list group for eaps at Alumni Stadium

--- hcat_app/app/alumnistadium/page.jsx
+++ hcat_app/app/alumnistadium/page.jsx
@@ -1,26 +1,28 @@
 // Creates the page for the Alumni Stadium EAPs
+//Dynamically updates EAP information with a List Group
 
 import data from "/components/data/data.json";
+import Header from "/components/header";
+import ListGroup from "/components/ListGroup";
+
+let eaps = ["Head Injury", "Neck Injury", "Back Injury"];
 
 const AlumniStadium = () => {
   return (
     <>
       <div>
+        <Header />
+      </div>
+      <div>
         <div>Venue: {data.venue}</div>
         <div>Sport: {data.sport}</div>
         <div>Address: {data.address}</div>
         <div>
-          <label>EAP Type:</label>
-          <select>
-            {data.actionPlan.map((actionPlan) => {
-              return (
-                <option key={actionPlan.id} value={actionPlan.id}>
-                  {actionPlan.name}
-                </option>
-              );
-            })}
-          </select>
-          <script>getElementById</script>
+          <ListGroup
+            className="eaps"
+            items={eaps}
+            heading="Emergency Action Plans"
+          />
         </div>
       </div>
     </>
