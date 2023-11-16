ffa6718f0a14a2749714f456c119eb1114f14045
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2023-11-16 12:46:43 -0500

    can pull data from json using id

--- hcat_app/app/locations/[locationId]/page.jsx
+++ hcat_app/app/locations/[locationId]/page.jsx
@@ -1,3 +1,13 @@
+import { location, planTypes } from "/data/locationData.json";
+
 export default function Locations({ params }) {
-  return <h1>Details about {params.locationId}</h1>;
+  const found = location.find((element) => element.id == params.locationId);
+
+  if (!found) {
+    return <h1>404 - Page Not Found</h1>;
+  }
+
+  console.log(found);
+
+  return <h1>{JSON.stringify(found)}</h1>;
 }
