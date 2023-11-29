29afb5b2f0477b35586064ca6011e9c4f08b9ece
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2023-11-29 15:38:08 -0500

    deleted console.log for found

--- hcat_app/app/locations/[locationId]/planTypes/[planTypeId]/page.jsx
+++ hcat_app/app/locations/[locationId]/planTypes/[planTypeId]/page.jsx
@@ -1,9 +1,24 @@
 import { location, planTypes } from "/data/locationData.json";
+import Header from "/components/header";
 
 export default function PlanType({ params }) {
+  const foundPlan = planTypes.find(
+    (element) => element.id == params.planTypeId
+  );
+  const foundLoc = location.find((el) => el.id == params.locationId);
+
+  if (!foundPlan) {
+    return <h1>404 - Page Not Found</h1>;
+  }
+
   return (
-    <h1>
-      PlanType {params.planTypeId} for location {params.locationId}
-    </h1>
+    <>
+      <Header />
+      <h1>{foundLoc.name}</h1>
+      <p>{foundLoc.sport}</p>
+      <p>{foundLoc.address}</p>
+      <h2>{foundPlan.name}</h2>
+      <p>{foundPlan.description}</p>
+    </>
   );
 }
