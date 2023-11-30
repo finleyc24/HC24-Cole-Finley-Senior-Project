f708306433a170eb28ea59a38e1965f700ab541f
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2023-11-30 00:05:37 -0500

    Edit the layout for background image

--- hcat_app/app/layout.jsx
+++ hcat_app/app/layout.jsx
@@ -1,20 +1,27 @@
-// Creates basic layout for the Home Page
+// Imports the necessary styles
 import "/styles/globals.css";
 
+// Defines metadata for the page
 export const metadata = {
   title: "Hanover College",
   description: "Find Hanover Emergency Action Plans",
 };
 
+// Root layout component
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        {/* Include any additional head elements such as meta tags, stylesheets, etc. */}
+        <link rel="stylesheet" href="/styles/globals.css" />
+      </head>
       <body>
+        {/* Main container with background image */}
         <div className="main">
           <div className="gradient" />
+          {/* Content container */}
+          <main className="app">{children}</main>
         </div>
-
-        <main className="app">{children}</main>
       </body>
     </html>
   );
