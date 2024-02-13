bd872beee49cea077b98fc9f28026137702277c0
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-02-13 11:30:06 -0500

    Added documentation comments to Policy page code

--- hcat_app/app/policies/[policyId]/page.jsx
+++ hcat_app/app/policies/[policyId]/page.jsx
@@ -1,15 +1,27 @@
-// Creates the page for policies
+/**
+ * This is a React component that creates the page for all the policies.
+ * It Imports the data from a Policy JSON file using each policies correct ID.
+ * After it imports it organizes the data from the JSON file onto the page for the user to view it
+ *
+ * @param {Object} params - The parameter passed to this component.
+ * @param {string} param.policyId - The ID of the policy that is meant to be displayed
+ *
+ * @returns {JSX.Element} - The React Component that is the policies page.
+ */
 import React from "react";
 import { policies } from "/data/policyData.json";
 import Header from "/components/header";
 
 export default function Policies({ params }) {
+  // Finds the policy witht the specific ID from the PolicyData.json file.
   const found = policies.find((element) => element.id == params.policyId);
 
+  // If a mathing policy isnt found the NextJS error page is returned.
   if (!found) {
     return <ErrorPage statusCode={404} />;
   }
 
+  // Creates the policy page using the policy details found in polisyData.json
   return (
     <>
       <Header />
