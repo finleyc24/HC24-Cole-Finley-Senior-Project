b2db301f695731e5c28e392910e1d9fdbaa7fc70
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-02-13 11:56:30 -0500

    added documentation comments to the platypes page

--- hcat_app/app/locations/[locationId]/planTypes/[planTypeId]/page.jsx
+++ hcat_app/app/locations/[locationId]/planTypes/[planTypeId]/page.jsx
@@ -1,18 +1,34 @@
+/**
+ * React component to create a page for details of a specific plan type.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {Object} props.params - The parameters passed to the component.
+ * @param {string} props.params.locationId - The ID of the location associated with the plan type.
+ * @param {string} props.params.planTypeId - The ID of the plan type to be displayed.
+ *
+ * @returns {JSX.Element} - The React component to create the plan type page.
+ */
+
 "use client";
 import { location, planTypes } from "/data/locationData.json";
 import Header from "/components/header";
 import ErrorPage from "next/error";
 
 export default function PlanType({ params }) {
+  // Find the plantype using the plantype Id from the json file.
   const foundPlan = planTypes.find(
     (element) => element.id == params.planTypeId
   );
+
+  // Find the location using the location id from the json file.
   const foundLoc = location.find((el) => el.id == params.locationId);
 
+  // If no location or plan type found return the NextJS error page.
   if (!foundLoc) {
     return <ErrorPage statusCode={404} />;
   }
 
+  // Create the plantype page that has a header, locations, and plantype details.
   return (
     <>
       <Header />
