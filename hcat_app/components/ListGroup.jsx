9c2e70430f30651a6fdc5e88df9d148c048d4fe8
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-02-13 11:42:10 -0500

    ListGroup Documentation Comments

--- hcat_app/components/ListGroup.jsx
+++ hcat_app/components/ListGroup.jsx
@@ -1,10 +1,21 @@
-// Creates a list group that can be used on each page
+/**
+ * React component that crates the list group so it can be used on any page
+ *
+ * @param {Object} props - The properties passed to the compnent
+ * @param {string[]} props.items - The array of items that should be displayed in the list
+ * @param {string} props.heading - The Title above the lis group
+ *
+ * @returns {JSX.Element} - The React component for a list group
+ */
+
 "use client";
 import { useState } from "react";
 
 function ListGroup({ items, heading }) {
+  // State to keep track of the seceted item
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
+  // create the list group with the items and selection function
   return (
     <div>
       <h1 style={{ color: "white" }}>{heading}</h1>
